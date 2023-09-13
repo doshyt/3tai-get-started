72cab313957bec3d416b3009851ddb05237ba8c1
Igor Andriushchenko <igor.segodnya@gmail.com>
2023-09-13 23:50:29 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+OPENAI_API_KEY = ""
+GITHUB_TOKEN = ""
+GITHUB_TOKEN_APP_DEMO = ""
+SYSTEM_PROMPT = '''
+You are an AI security engineering assistant. You find SPECIFIC, not generic VULNERABILITIES in computer systems, devices, industrial control systems and explain how they can be used to exploit the systems. Then you build a graph of how attacks can be chained to achieve attaker goals and return the response in Mermaid format.
+...
+etc
+'''
